80e08ff174670157fbe58431c8cbf35e599d072b
MonsieuBili <raulflorin2005@gmail.com>
2026-05-14 18:07:33 +0300

    Introduced TurnController

---
+++ src/main/java/org/example/issproject/controller/TurnController.java
@@ -0,0 +1,35 @@
+package org.example.issproject.controller;
+
+import lombok.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/turn")
+@RequiredArgsConstructor
+public class TurnController {
+
+    @Value("${metered.secret.key}")
+    private String secretKey;
+
+    @GetMapping("/credentials")
+    public ResponseEntity<?> getCredentials() throws Exception {
+        String username = String.valueOf(System.currentTimeMillis() / 1000 + 3600);
+
+        Mac mac = Mac.getInstance("HmacSHA1");
+        mac.init(new SecretKeySpec(secretKey.getBytes(), "HmacSHA1"));
+        String credential = Base64.getEncoder()
+                .encodeToString(mac.doFinal(username.getBytes()));
+
+        return ResponseEntity.ok(Map.of(
+                "username", username,
+                "credential", credential,
+                "url", "turn:issproject.metered.live:80"
+        ));
+    }
+}
