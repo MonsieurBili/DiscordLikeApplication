19bbe2648315212b0ea3b2eb5151361885466a74
MonsieuBili <raulflorin2005@gmail.com>
2026-05-14 18:53:47 +0300

    Introduced TurnController fixed

--- src/main/java/org/example/issproject/controller/TurnController.java
+++ src/main/java/org/example/issproject/controller/TurnController.java
@@ -3,6 +3,7 @@ package org.example.issproject.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/turn")
+@CrossOrigin(origins = {"http://localhost:5173","https://uiiss.onrender.com/"})
 @RequiredArgsConstructor
 public class TurnController {
 
