cd79f9e6733de421d82bf719dbc09eef471bf79b
MonsieuBili <raulflorin2005@gmail.com>
2026-05-14 18:12:10 +0300

    Introduced TurnController fixed

--- src/main/java/org/example/issproject/controller/TurnController.java
+++ src/main/java/org/example/issproject/controller/TurnController.java
@@ -1,8 +1,11 @@
 package org.example.issproject.controller;
 
-import lombok.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
